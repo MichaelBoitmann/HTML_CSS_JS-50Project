e3621f1f885f12fe395098f5240e16050a7fdf18
Michael Boitmann <michael_boitmann@yahoo.com>
2022-03-28 23:34:44 +1300

    created the html and CSS style to format the photo positioning with arrows

--- Day17_Movie_App/script.js
+++ Day17_Movie_App/script.js
@@ -1,6 +1,6 @@
-const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=1014a819f9aef745ec25e4d9501f00bc&page=1'
+const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=1014a819f9aef745ec25e4d9501f00bc&page=2'
 const IMG_PATH = 'https://image.tmdb.org/t/p/w1280'
-const SEARCH_API = 'https://api.themoviedb.org/3/discover/movie?api_key=1014a819f9aef745ec25e4d9501f00bc&query="'
+const SEARCH_API = 'https://api.themoviedb.org/3/search/movie?api_key=1014a819f9aef745ec25e4d9501f00bc&query="'
 
 const main = document.getElementById('main')
 const form = document.getElementById('form')
@@ -12,7 +12,7 @@ getMovies(API_URL)
 async function getMovies(url) {
     const res = await fetch(url)
     const data = await res.json()
-
+    console.log(data)
     showMovies(data.results)
 }
 
@@ -26,16 +26,17 @@ function showMovies(movies) {
         movieEl.classList.add('movie')
 
         movieEl.innerHTML = `
-            <img src="${IMG_PATH + poster_path}" alt="${title}">
-            <div class="movie-info">
-          <h3>${title}</h3>
-          <span class="${getClassByRate(vote_average)}">${vote_average}</span>
-            </div>
-            <div class="overview">
-          <h3>Overview</h3>
-          ${overview}
+        <img src="${IMG_PATH + poster_path}" alt="${title}">
+        <div class="movie-info">
+            <h3>${title}</h3>
+            <span class="${getClassByRate(vote_average)}">${vote_average}</span>
+        </div>
+        <div class="overview">
+        <h3>Overview</h3>
+        ${overview}
         </div>
         `
+
         main.appendChild(movieEl)
     })
 }
@@ -43,7 +44,7 @@ function showMovies(movies) {
 function getClassByRate(vote) {
     if(vote >= 8) {
         return 'green'
-    } else if(vote >= 5) {
+    } else if(vote >=5) {
         return 'orange'
     } else {
         return 'red'
@@ -62,4 +63,5 @@ form.addEventListener('submit', (e) => {
     } else {
         window.location.reload()
     }
+    
 })
