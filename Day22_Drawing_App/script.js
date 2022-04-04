1fdbd0982a17ca88540f7285953f7b5b5f6c4e82
Michael Boitmann <michael_boitmann@yahoo.com>
2022-04-04 21:33:13 +1200

    added elements of increase, decrease, size, color and clear. created the colorEl that will change of color of the drawing tip

--- Day22_Drawing_App/script.js
+++ Day22_Drawing_App/script.js
@@ -1,10 +1,47 @@
+// Referenced to Canvas API that provides the means for drawing
+// drawing graphics via JavaScrip and HTML <canvas> element.
+// link (https://developer.mozilla.org/en-US/docs/Web/API/Canvas_API)
+
 const canvas = document.getElementById('canvas');
+const increaseBtn = document.getElementById('increase');
+const decreaseBtn = document.getElementById('decrease');
+const sizeEl = document.getElementById('size');
+const colorEl = document.getElementById('color');
+const clearEl = document.getElementById('clear');
+
 const ctx = canvas.getContext('2d');
 
 let size = 20
 let color = 'black'
 let x
 let y
+
+canvas.addEventListener('mousedown', (e) => {
+    isPressed = true
+
+    x = e.offsetX
+    y = e.offsetY
+})
+
+document.addEventListener('mouseup', (e) => {
+    isPressed = false
+
+    x = undefined
+    y = undefined
+})
+
+canvas.addEventListener('mousemove', (e) => {
+    if(isPressed) {
+        const x2 = e.offsetX
+        const y2 = e.offsetY
+
+        drawCircle(x2, y2)
+        drawLine(x, y, x2, y2)
+
+        x = x2
+        y = y2
+    }
+})
 
 function drawCircle(x, y) {
     ctx.beginPath();
@@ -18,6 +55,9 @@ function drawLine(x1, y1, x2, y2) {
     ctx.moveTo(x1, y1)
     ctx.lineTo(x2, y2)
     ctx.strokeStyle = color
-    ctx.lineWidth = size
+    ctx.lineWidth = size * 2
     ctx.stroke()
 }
+
+
+colorEl.addEventListener('change', (e) => color = e.target.value)
