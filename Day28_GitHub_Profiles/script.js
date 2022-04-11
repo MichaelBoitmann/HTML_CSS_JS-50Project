31fbece80ce30d13d8379f1114ae137465ae6b58
Michael Boitmann <michael_boitmann@yahoo.com>
2022-04-11 23:03:38 +1200

    worked on script that will do the search of the name to be posted on the card

---
+++ Day28_GitHub_Profiles/script.js
@@ -0,0 +1,26 @@
+const APIURL = 'https://api.github.com/users/'
+
+const form = document.getElementById('form')
+const search = document.getElementById('search')
+
+async function getUser(username) {
+    try {
+        const res = await axios(APIURL + username)
+
+        console.log(res.data)
+    } catch(err) {
+        console.log(err)
+    } 
+}
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const user = search.value
+
+    if(user) {
+        getUser(user.id)
+        
+        search.value = ''
+    }
+})
