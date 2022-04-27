3e7bc8bee7ff395fe3f0e0296168854544de9c6e
Michael Boitmann <michael_boitmann@yahoo.com>
2022-04-28 00:15:59 +1200

    added function that wil create a pokemon card

--- Day37_Pokedex/script.js
+++ Day37_Pokedex/script.js
@@ -19,16 +19,17 @@ const colors = {
 	normal: '#F5F5F5'
 }
 
-const fetchPokemons = () => {
-    for(let i = 1; i <= pokemon_count; i++) {
+const main_types = Object.keys(colors)
+
+const fetchPokemons = async () => {
+    for(let i = 1; i <= pokemon_count; i++)
         await getPokemon(i)
-    }
 }
 
 const getPokemon = async (id) => {
     const url = `https://pokeapi.co/api/v2/pokemon/${id}`
     const res = await fetch(url)
-    const data = await res.jason()
+    const data = await res.json()
     createPokemonCard(data)
 }
 
@@ -36,14 +37,23 @@ const createPokemonCard = (pokemon) => {
     const pokemonEl = document.createElement('div')
     pokemonEl.classList.add('pokemon')
 
+    const name = pokemon.name[0].toUpperCase() + pokemon.name.slice(1)
+    const id = pokemon.id.toString().padStart(3, '0')
+
+    const poke_types = pokemon.types.map(type => type.type.name)
+    const type = main_types.find(type => poke_types.indexOf(type) > -1)
+    const color = colors[type]
+
+    pokemonEl.style.backgroundColor = color
+
     const pokemonInnerHTML = `
     <div class="img-container">
-        <img src="https://pokeres.bastionbot.org/images/pokemon/${pokemon.id}" alt="">
+        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png"" alt="${name}">
     </div>
     <div class="info">
-        <span class="number">#001</span>
-        <h3 class="name">Bulbasaur</h3>
-        <small class="type">Type: <span>grass</span></small>
+        <span class="number">#${id}</span>
+        <h3 class="name">${name}</h3>
+        <small class="type">Type: <span>${type}</span> </small>
     </div>
     `
 
@@ -51,3 +61,5 @@ const createPokemonCard = (pokemon) => {
 
     poke_container.appendChild(pokemonEl)
 }
+
+fetchPokemons()
