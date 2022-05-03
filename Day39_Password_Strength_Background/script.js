73a4402da88af45421fbf2a7e9d1a55f40b711a1
Michael Boitmann <michael_boitmann@yahoo.com>
2022-05-03 17:39:00 +1200

    created script that will create the box container that holds the boxes for 16 and rotate when click

--- Day39_Password_Strength_Background/script.js
+++ Day39_Password_Strength_Background/script.js
@@ -4,6 +4,6 @@ const background = document.getElementById('background')
 password.addEventListener('input', (e) => {
   const input = e.target.value
   const length = input.length
-  const blurValue = 20 - length * 2
+  const blurValue = 30 - length * 2
   background.style.filter = `blur(${blurValue}px)`
 })
