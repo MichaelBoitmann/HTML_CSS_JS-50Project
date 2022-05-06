85609b0f6d9263499ed6a1cc5721e67c6ced3da4
Michael Boitmann <michael_boitmann@yahoo.com>
2022-05-06 22:38:43 +1200

    added some time on api

--- Day42_Live_User_Filter/script.js
+++ Day42_Live_User_Filter/script.js
@@ -4,8 +4,11 @@ const listItems = []
 
 getData()
 
+filter.addEventListener('input', (e) => firlterData(e.target.value))
+
+
 async function getData() {
-    const res = await fetch('https://randomuser.me/api?results=50')
+    const res = await fetch('https://randomuser.me/api?results=500')
 
     const { results } = await res.json()
 
@@ -13,6 +16,29 @@ async function getData() {
     results.innerHTML = ''
 
     results.forEach( user => {
-        console.log(user)
+        const li = document.createElement('li')
+
+        listItems.push(li)
+
+        li.innerHTML = `
+            <img src="${user.picture.large}" alt="${user.name.first}">
+            <div class="user-info">
+                <h4>${user.name.title} ${user.name.first} ${user.name.last}</h4>
+                <p>${user.email}</p>
+                <p>${user.location.city}, ${user.location.country}</p>
+            </div>
+        `
+
+        result.appendChild(li)
+    })
+}
+
+function filterData(searchTerm) {
+    listItems.forEach(liveUser => {
+        if(liveUser.innerText.toLowerCase().includes(searchTerm.toLowerCase())) {
+            liveUser.classList.remove('hide')
+        } else {
+            liveUser.classList>add('hide')
+        }
     })
 }
